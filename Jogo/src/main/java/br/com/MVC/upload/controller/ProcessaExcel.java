828d98434aabd3e97ea7c4664bf1f1c550c58b18
Claudinei de Souza <claudineidesenvolvedor@gmail.com>
2023-11-18 15:45:37 -0400

    Update ProcessaExcel.java

--- Jogo/src/main/java/br/com/MVC/upload/controller/ProcessaExcel.java
+++ Jogo/src/main/java/br/com/MVC/upload/controller/ProcessaExcel.java
@@ -10,6 +10,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.DataFormat;
+import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -50,17 +51,17 @@ public class ProcessaExcel {
 		// set font 1 to 12 point type
 		f.setFontHeightInPoints((short) 12);
 		// make it blue
-		f.setColor((short) 0xc);
+		f.setColor((short) 10);
 		// make it bold
 		// arial is the default font
-		f.setBoldweight(Font.BOLDWEIGHT_BOLD);
+		f2.setBold(true);
 
 		// set font 2 to 10 point type
 		f2.setFontHeightInPoints((short) 10);
 		// make it red
 		f2.setColor((short) Font.COLOR_RED);
 		// make it bold
-		f2.setBoldweight(Font.BOLDWEIGHT_BOLD);
+		f2.setBold(true);
 
 		f2.setStrikeout(true);
 
@@ -70,9 +71,9 @@ public class ProcessaExcel {
 		cs.setDataFormat(df.getFormat("#,##0.0"));
 
 		// set a thin border
-		cs2.setBorderBottom(cs2.BORDER_THIN);
+		cs2.setBorderBottom(cs2.getBorderTop());
 		// fill w fg fill color
-		cs2.setFillPattern((short) CellStyle.SOLID_FOREGROUND);
+		cs2.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 		// set the cell format to text see DataFormat for a full list
 		cs2.setDataFormat(HSSFDataFormat.getBuiltinFormat("text"));
 
@@ -82,6 +83,7 @@ public class ProcessaExcel {
 		// set the sheet name in Unicode
 		wb.setSheetName(0, "\u0422\u0435\u0441\u0442\u043E\u0432\u0430\u044F "
 				+ "\u0421\u0442\u0440\u0430\u043D\u0438\u0447\u043A\u0430");
+		
 		// in case of plain ascii
 		// wb.setSheetName(0, "HSSF Test");
 		// create a sheet with 30 rows (0-29)
@@ -133,7 +135,7 @@ public class ProcessaExcel {
 
 		// define the third style to be the default
 		// except with a thick black border at the bottom
-		cs3.setBorderBottom(cs3.BORDER_THICK);
+		cs3.setBorderBottom(cs3.getBorderTop());
 
 		// create 50 cells
 		for (short cellnum = (short) 0; cellnum < 50; cellnum++) {
